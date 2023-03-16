71689170b963cbd2f706b9e1bf61f06635a7903c
ahmar7 <ahmarjabbar7@gmail.com>
2023-03-16 13:48:48 +0500

    boiler plate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,9 @@
 import React from "react";
-import Banner from "../components/Home/Banner";
-import Gifts from "../components/Home/Gifts";
-import HowItWorks from "../components/Home/HowItWorks";
-import Footer from "../layout/Footer/Footer";
-import Header from "../layout/Header/Header";
+// import Footer from "../layout/Footer/Footer";
+// import Header from "../layout/Header/Header";
 
 const Home = () => {
-  return (
-    <div className="home-page">
-      <Header />
-      <Banner />
-      <Gifts />
-      <HowItWorks />
-      <Footer />
-    </div>
-  );
+  return <div className="home-page">{/* <Header /> */}Ahmar</div>;
 };
 
 export default Home;
