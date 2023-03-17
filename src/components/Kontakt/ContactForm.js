afad140c56015e5ddd10134d8edc7b1621822e3b
ahmar7 <ahmarjabbar7@gmail.com>
2023-03-17 15:00:26 +0500

    update

--- src/components/Kontakt/ContactForm.js
+++ src/components/Kontakt/ContactForm.js
@@ -7,8 +7,54 @@ const ContactForm = () => {
         <div className="contact-flex">
           <div className="contact-detail">
             <h3>Adresse</h3>
+            <p className="location">
+              <i class="fa-solid fa-location-dot"></i> stiory berf, 4600 koga
+            </p>
+            <div className="info-flex">
+              <div className="info-left">
+                <p>Kontakt information</p>
+                <ul>
+                  <li>
+                    <i class="fa-solid fa-phone "></i> 23151502
+                  </li>
+                  <li>
+                    <i class="fa-solid fa-mobile "></i> 66121202
+                  </li>
+                  <li>
+                    <i class="fa-solid fa-envelope"></i> info@dithjem.dk
+                  </li>
+                </ul>
+              </div>
+              <div className="info-left">
+                <p>Abningstider</p>
+                <ul>
+                  <li>
+                    <i class="fa-solid fa-clock "></i> Man - fre : 06 til 20
+                  </li>
+                  <li>
+                    <i
+                      class="fa-solid fa-clock "
+                      style={{ visibility: "hidden" }}
+                    ></i>{" "}
+                    Lor - Son : 09 til 14
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="contact-form"></div>
+          <div className="contact-form">
+            <h3>Kontakt os</h3>
+            <div className="input-form">
+              <input type="text" placeholder="Navn*" />
+              <input type="email" placeholder="Email*" />
+              <input type="text" placeholder="tif" />
+              <textarea type="text" rows="8" placeholder="Besked*" />
+
+              <div className="con-btn">
+                <button className="se-mere">Send besked</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
